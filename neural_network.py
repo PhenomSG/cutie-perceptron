b07f571a9b3a803e021fb5631f683b21476d8b8a
Sahaj Gupta <sahajguptarocks@gmail.com>
2026-08-27 03:40:05 +0530

    doing it

---
+++ neural_network.py
@@ -0,0 +1,21 @@
+"""
+Below is a Perceptron(Single layer neuron) that solves "OR" problem
+
+Condition:
+    y = 0 if I < 0.5
+    y = 1 otherwise
+
+Training_Pattern    Attributes          Desired_Output
+                    Input_1 Input_2
+    1                   1       1           1
+    2                   0       1           1
+    3                   1       0           1
+    4                   0       0           0
+
+    
+
+Thought-process
+1. Build a 
+
+
+"""
